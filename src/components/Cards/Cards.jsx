a1280beabe71de13953b80be944d6ea9a2300a50
Gustavo Martins <tioben@outlook.com.br>
2025-10-02 11:04:03 -0300

    2025-10-02: Base dos componentes e instalação de pacotes

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,7 @@
+import styles from "./Cards.module.css";
+
+function Cards() {
+  return <div>card</div>;
+}
+
+export default Cards;
---
+++ src/components/Pages/Page1/Page1.jsx
@@ -0,0 +1,7 @@
+import styles from "./Page1.module.css";
+
+function Page1() {
+  return <div>Page1</div>;
+}
+
+export default Page1;
