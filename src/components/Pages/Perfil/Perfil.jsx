1f33b9f9e648deafd0460daf707ff21309498ff5
Gustavo Martins <tioben@outlook.com.br>
2025-10-02 18:43:28 -0300

    2025-10-02: Edit Perfil Pop UP

--- src/components/Pages/Perfil/Perfil.jsx
+++ src/components/Pages/Perfil/Perfil.jsx
@@ -12,6 +12,7 @@ function Perfil() {
   const [vitorias, setVitorias] = useState(0);
   const [cards, setCards] = useState(0);
   const [skins, setSkins] = useState(0);
+  const [perfilEditPopUP, setPerfilEditPopUP] = useState("none");
 
   return (
     <div className={styles.Perfil_Main_Container}>
@@ -26,7 +27,48 @@ function Perfil() {
         <Perfil_Card num={cards} title={"Cards"} />
         <Perfil_Card num={skins} title={"Skins"} />
       </div>
-      <button>Editar Perfil</button>
+      <div
+        className={styles.Perfil_Edit_Pop_UP}
+        style={{ display: perfilEditPopUP }}
+      >
+        <div className={styles.Perfil_Edit_left}>
+          <ul type="none">
+            <li>
+              <input type="radio" name="perfilEditPopUP" id="icon" />
+              <label htmlFor="perfilEditPopUP">Ícones</label>
+            </li>
+            <li>
+              <input type="radio" name="perfilEditPopUP" id="background" />
+              <label htmlFor="perfilEditPopUP">Backgrounds</label>
+            </li>
+            <li>
+              <input type="radio" name="perfilEditPopUP" id="effect" />
+              <label htmlFor="perfilEditPopUP">Efeitos</label>
+            </li>
+            <li>
+              <input type="radio" name="perfilEditPopUP" id="colors" />
+              <label htmlFor="perfilEditPopUP">Cores</label>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.Perfil_Edit_Right}>
+          <button></button>
+          <div>area</div>
+          <button></button>
+        </div>
+        <button
+          className={styles.Perfil_Edit_Close}
+          onClick={() => setPerfilEditPopUP("none")}
+        >
+          X
+        </button>
+      </div>
+      <button
+        className={styles.Perfil_Edit_Open}
+        onClick={() => setPerfilEditPopUP("flex")}
+      >
+        Editar Perfil
+      </button>
     </div>
   );
 }
