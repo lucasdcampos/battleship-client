e2afb02da2cb43bd29417b66da53504daa56641b
Levi313 <levicoimbra31@gmail.com>
2025-10-04 15:17:11 -0300

    2025-10-04 Edição-navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,17 @@ import Header from "./components/Pages/Header/Header";
 import Play from "./components/Pages/Play/Play";
 import Perfil from "./components/Pages/Perfil/Perfil";
 import Store from "./components/Pages/Store/Store";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <Router>
-        {/* <Navbar /> */}
-        <Header />
-        <Routes>
-          <Route exact path="/" element="" />
-
-          <Route path="/Play" element={<Play />} />
-
-          <Route path="/Perfil" element={<Perfil />} />
-
-          <Route path="/Store" element={<Store />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/Play" element={<Play />} />
+        <Route path="/Perfil" element={<Perfil />} />
+        <Route path="/Store" element={<Store />} />
+      </Routes>
+    </Router>
   );
 }
 
