609afea705a20d8a76ade2494b8f6c7f1670b4f8
Gustavo Martins <tioben@outlook.com.br>
2025-10-06 15:07:36 -0300

    2025-10-06: Tela de login e navegação

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Header from "./components/Pages/Header/Header";
 import Play from "./components/Pages/Play/Play";
 import Perfil from "./components/Pages/Perfil/Perfil";
 import Store from "./components/Pages/Store/Store";
-import Login from "./components/Pages/Formulario/Login";
-import Signup from "./components/Pages/Formulario/Signup";
+import Login from "./components/Pages/Login/Login";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/Login" element={<Login />} />
-        <Route path="/Signup" element={<Signup />} />
         <Route path="/Play" element={<Play />} />
         <Route path="/Perfil" element={<Perfil />} />
         <Route path="/Store" element={<Store />} />
---
+++ src/components/Pages/Login/Login.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import styles from "./Login.module.css";
+import navio1 from "../../../assets/navios/animacao_tela_de_login/navio1.png";
+import navio2 from "../../../assets/navios/animacao_tela_de_login/navio2.png";
+import navio3 from "../../../assets/navios/animacao_tela_de_login/navio3.png";
+import navio4 from "../../../assets/navios/animacao_tela_de_login/navio4.png";
+
+function LoginForm({ onSwitchForm }) {
+  const handleSubmitLoginForm = (e) => {
+    e.preventDefault();
+    console.log("Login submit");
+  };
+  const handleSubmitSignupForm = (e) => {
+    e.preventDefault();
+    console.log("Signup submit");
+  };
+  const [actualTab, setActualTab] = useState("login");
+
+  return (
+    <div className={styles.login_background}>
+      <img src={navio1} alt="navio1" className={styles.navio1} />
+      <img src={navio2} alt="navio2" className={styles.navio2} />
+      <img src={navio3} alt="navio3" className={styles.navio3} />
+      <img src={navio4} alt="navio4" className={styles.navio4} />
+
+      <h2 className={styles.form_title}>Acesse sua conta</h2>
+      <div className={styles.login_container}>
+        {/* Logar em conta existente */}
+        <form
+          onSubmit={handleSubmitLoginForm}
+          style={{ display: actualTab === "login" ? "flex" : "none" }}
+        >
+          <input type="email" placeholder="E-mail" required />
+          <input type="password" placeholder="Senha" required />
+          <button
+            type="button"
+            className={styles.text_link_stacked_link}
+            onClick={() => setActualTab("forgot_password")}
+          >
+            Esqueceu a senha?
+          </button>
+          <button
+            type="button"
+            className={styles.text_link_stacked_link}
+            onClick={() => setActualTab("new_account")}
+          >
+            Criar nova conta
+          </button>
+          <button
+            type="submit"
+            className={styles.main_button}
+            onClick={() => handleSubmitLoginForm()}
+          >
+            Entrar
+          </button>
+        </form>
+        {/* Criar nova conta */}
+        <form
+          onSubmit={handleSubmitSignupForm}
+          style={{ display: actualTab === "new_account" ? "flex" : "none" }}
+        >
+          <input type="text" placeholder="Nome de usuário" required />
+          <input type="email" placeholder="E-mail" required />
+          <input type="password" placeholder="Senha" required />
+          <input type="password" placeholder="Confirmar senha" required />
+          <button
+            type="button"
+            className={styles.text_link_stacked_link}
+            onClick={() => setActualTab("login")}
+          >
+            Já tem uma conta? Entrar
+          </button>
+          <button
+            type="submit"
+            className={styles.main_button}
+            onClick={() => handleSubmitSignupForm()}
+          >
+            Criar
+          </button>
+        </form>
+        {/* Recuperar senha */}
+        <form
+          onSubmit={handleSubmitSignupForm}
+          style={{ display: actualTab === "forgot_password" ? "flex" : "none" }}
+        >
+          <input type="email" placeholder="E-mail" required />
+          <button
+            type="button"
+            className={styles.text_link_stacked_link}
+            onClick={() => setActualTab("login")}
+          >
+            Voltar
+          </button>
+          <button
+            type="submit"
+            className={styles.main_button}
+            onClick={() => handleSubmitRecoveryForm()}
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
